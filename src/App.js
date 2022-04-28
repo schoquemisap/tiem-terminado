d95164c48cf5f15af47ef53e5328c609fdbe8033
schoquemisap <ivaljch_@hotmail.com>
2022-04-28 09:54:45 -0400

    tine final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TimeTracker from './timeTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <TimeTracker />
+    </div>
+  );
+}
+
+export default App;
